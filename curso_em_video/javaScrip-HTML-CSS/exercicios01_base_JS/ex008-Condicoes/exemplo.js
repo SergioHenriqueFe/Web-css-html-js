383a35478ac80ae5943c6153f39f986d41bd771e
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-05-15 18:12:15 -0300

    DOM, Condições

---
+++ curso_em_video/javaScrip-HTML-CSS/exercicios01_base_JS/ex008-Condicoes/exemplo.js
@@ -0,0 +1,10 @@
+var velocidade = 12
+console.log(`sua velocidade é de ${velocidade} Km/h`)
+var conversao = velocidade/3.6
+console.log(`sua velocidade é de ${conversao}m/s `)
+
+if (velocidade > 36){
+    console.log(`Você foi multado`)
+}else {
+    console.log(`Não teve multa`)
+}
